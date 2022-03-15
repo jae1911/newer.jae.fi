855e868038342aa44c4dbdc1fbbd3d67c2b61dfd
Jae Lo Presti (DN0) <me@jae.fi>
2022-03-15 22:50:24 +0200

    Config: patch standalone folder not generated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
   experimental: {
     outputStandalone: true,
   },
-};
-
-module.exports = nextConfig
+}
