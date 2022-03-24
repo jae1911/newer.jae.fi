3622314338eaf4c0adf2be3e3daef944f351619c
Jae Lo Presti (DN0) <me@jae.fi>
2022-03-24 11:18:30 +0200

    Config: revert to default config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  experimental: {
-    outputStandalone: false,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = {
+  async rewrites() {
+    return [
+      // Redir everything to / because cool one page thing
+      {
+        source: '/:path',
+        destination: ':',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig
