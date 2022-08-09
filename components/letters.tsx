dd07e533c96f13b9f9c891279c99a43f108ef0e1
Jae Lo Presti (DN0) <me@jae.fi>
2022-08-09 12:39:46 +0300

    Components: add alt property to images

--- components/letters.tsx
+++ components/letters.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Alphabet(letter) {
     let te = "/svgfont/" + letter.letter + ".svg";
+    let altText = "Letter " + letter.letter;
 
     return(
-        <Image width="30" height="30" src={te}/>
+        <Image width="30" height="30" alt={altText} src={te}/>
     )
 }
